491851981b8430ce3e24c9fd009ddf354cd1b199
Dibss <ludovicad01@gmail.com>
2022-08-24 23:45:51 +0200

    meal item structure

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,25 @@
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1'
+        }}
+        label="Amount"
+      />
+      <button type="submit" id="addToCart">
+        + Add
+      </button>
+    </form>
+  );
+};
+
+export default MealItemForm;
