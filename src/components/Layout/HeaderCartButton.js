a4f4ead783e668721b545444ad292d004cfa6366
Dibss <ludovicad01@gmail.com>
2022-08-25 02:17:39 +0200

    added animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,45 @@
-import CartIcon from '../Cart/CartIcon';
-import classes from './HeaderCartButton.module.css';
+import { useContext, useEffect, useState } from "react";
 
-const HeaderCartButton = props => {
+import CartContext from "../../store/cart-context";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  const [btnHighlight, setBtnHighlight] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
+  const itemsNumber = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${btnHighlight ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnHighlight(true);
+
+    const timer = setTimeout(() => {
+      setBtnHighlight(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>
-        3 (dummy value)
-      </span>
+      <span className={classes.badge}>{itemsNumber}</span>
     </button>
-  )
-}
+  );
+};
 
 export default HeaderCartButton;
