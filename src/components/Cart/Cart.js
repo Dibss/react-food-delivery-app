bf0b638e76e9c7fc7ce45d62795e20f77dea1d13
Dibss <ludovicad01@gmail.com>
2022-08-25 02:16:41 +0200

    added cart item comp

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,32 +1,51 @@
-import Modal from '../UI/Modal';
-import classes from './Cart.module.css';
+import { useContext } from "react";
+
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const addItemToCartHandler = (item) => {
+    cartCtx.additem(item);
+  };
 
   const cartItems = [
-    {
-      id: 'c1',
-      name: 'Sushi',
-      amount: '2',
-      price: '12.99'
-    }.map(item => <li>{item.name}</li>)
+    cartCtx.items.map((item) => (
+      <CartItem
+        key={item.id}
+        name={item.name}
+        amount={item.amount}
+        price={item.price}
+        onAdd={addItemToCartHandler.bind(null, item.id)}
+        onRemove={cartItemRemoveHandler.bind(null, item.id)}
+      />
+    )),
   ];
 
   return (
-    <Modal>
-      <ul className={classes['cart-items']}>
-        {cartItems}
-      </ul>
+    <Modal onClose={props.onCloseCart}>
+      <ul className={classes["cart-items"]}>{cartItems}</ul>
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>{}</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onCloseCart}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
-  )
+  );
 };
 
 export default Cart;
