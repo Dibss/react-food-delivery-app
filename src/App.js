4253ca687b1a7bfd4fe74dd2837d39749204735b
Dibss <ludovicad01@gmail.com>
2022-08-25 02:16:19 +0200

    added cartcontextprovider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import { Fragment } from "react";
+import { useState } from "react";
 
+import CartProvider from "./store/CartProvider";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <Fragment>
-      <Cart />
-      <Header></Header>
+    <CartProvider>
+      {cartIsShown && <Cart onCloseCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler}></Header>
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
